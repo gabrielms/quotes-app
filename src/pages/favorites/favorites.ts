70999fbeff62d1343c108daf95349ea99ca2f79a
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2017-05-22 08:56:58 -0300

    Changes in favorites modal;

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -21,8 +21,14 @@ export class FavoritesPage {
   }
 
   onViewQuote(quote: Quote){
-    const modal = this.modalCtrl.create(QuotePage);
+    const modal = this.modalCtrl.create(QuotePage, quote);
 
     modal.present();
+    modal.onDidDismiss((remove: boolean) => {
+      if(remove){
+        this.quotesService.removeQuoteFromFavorites(quote);
+        this.quotes = this.quotesService.getFavoriteQuotes();
+      }
+    });
   }
 }
--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,24 +1,25 @@
+import { QuotesService } from './../../services/quotes';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  person: string;
+  text: string;
+  constructor(
+    private viewCtrl: ViewController,
+    private navParams: NavParams,
+    private quoteService: QuotesService){
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  ionViewDidLoad(){
+    this.person = this.navParams.get('person');
+    this.text = this.navParams.get('text');
   }
-
+  onClose(remove = false){
+    this.viewCtrl.dismiss(remove);
+  }
 }
